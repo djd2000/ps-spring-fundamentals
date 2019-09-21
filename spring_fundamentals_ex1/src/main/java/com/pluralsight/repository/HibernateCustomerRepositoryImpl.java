cf62102ba6b7b670840e2b92f2e790df4ca47d83
david <davidd125@yahoo.com>
2019-09-21 22:16:09 +0100

    XML Config injection

--- spring_fundamentals_ex1/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ spring_fundamentals_ex1/src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -3,10 +3,13 @@ package com.pluralsight.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import com.pluralsight.model.Customer;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
+	@Value("${dbusername}")
 	private String dbusername;
 
 	@Override
@@ -23,10 +26,6 @@ public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
 		return customers;
 
-	}
-
-	public void setDbusername(String dbusername) {
-		this.dbusername = dbusername;
 	}
 
 }
